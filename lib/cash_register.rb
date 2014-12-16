9ee6fda73a1a6e64cca7128f3c988376d4b04aac
roryg11 <Rory.C.Grant@gmail.com>
2014-12-15 19:24:12 -0800

    total, purchase, pay and starting balance of zero

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,20 @@
 class CashRegister
-  def total
-    0
+
+  def initialize
+    @total = 0
+    @pay = 0
   end
+
+  def total
+    @total
+  end
+
+  def purchase(amount)
+    @total = @total += amount.to_f
+  end
+
+  def pay(payment_price)
+    @pay = (@total -= payment_price).abs
+  end
+
 end
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -10,4 +10,47 @@ describe CashRegister do
       expect(total).to eq(0)
     end
   end
+
+  describe "#purchase" do
+    it 'adds an amount to the total' do
+      cash_register = CashRegister.new
+      cash_register.purchase(25.5)
+
+      total = cash_register.total
+
+      expect(total).to eq(25.5)
+    end
+
+    it 'adds multiple items to the total' do
+      cash_register = CashRegister.new
+      cash_register.purchase(25.5)
+      cash_register.purchase(5.5)
+
+      total = cash_register.total
+
+      expect(total).to eq(31.0)
+    end
+  end
+
+  describe "#pay" do
+    it 'takes in money from the purchaser to even balance' do
+      cash_register = CashRegister.new
+      cash_register.purchase(25.5)
+      cash_register.purchase(5.5)
+
+      change = cash_register.pay(31.0)
+
+      expect(change).to eq(0)
+    end
+
+    it 'takes in money from the purchaser and gives change' do
+      cash_register = CashRegister.new
+      cash_register.purchase(25.5)
+      cash_register.purchase(5.5)
+
+      change = cash_register.pay(32.5)
+
+      expect(change).to eq(1.5)
+    end
+  end
 end
